e8aa61b6b526aa05cf5da1650af928434dbe48d6
Goud <learing@gmail.com>
2025-07-16 11:44:09 +0530

    added 2 files 1.substring and 2.float

---
+++ Day1/Examples/float.py
@@ -0,0 +1,10 @@
+num1 = float(input("Please enter the number1  "))
+num2 = float(input("Please enter the number2  "))
+
+operation = input("Please select the operation you want to perfome: Add + ,Substration - , Mulitply *, Divison / ")
+
+if operation == "+":
+    print("sum of two numbers are: ", num1 + num2)
+elif operation == "-":
+    print("substartion of two numbeer are: ", num1 - num2)
+    
---
+++ Day1/Examples/substring.py
@@ -0,0 +1,6 @@
+text = "Python is easy to learn"
+
+substring = "is"
+
+if substring in text:
+    print(substring, "found in the text")
